3c8b3f057282d0157e1360f23bcb8b7179ae8d96
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2022-04-28 16:44:56 +0100

    1.4.0

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ async function run() {
     const cargoFile = core.getInput('cargoFile');
     const version = core.getInput('version');
     const buildNumberOnly = core.getInput('buildNumberOnly');
+    const overwriteCargoFile = core.getInput('overwriteCargoFile');
     core.info(`Running with ${cargoFile} ${version} ${buildNumberOnly}`);
     const result = setVersion(fs.readFileSync(cargoFile).toString(), version, buildNumberOnly);
+    
+    if (overwriteCargoFile) {
+      fs.writeFileSync(cargoFile, result.content);
+    }
+    
     core.setOutput('content', result.content);
     core.setOutput('version', result.version);
   } catch (error) {
